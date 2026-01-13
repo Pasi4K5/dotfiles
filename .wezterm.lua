675e5bc2142676cc1f0541353892ce3f5c5bb125
Pascal Pomper <pascal.pomper03@gmail.com>
2026-01-13 11:33:14 +0100

    Update wezterm configuration

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 config.initial_cols = 120
 config.initial_rows = 28
-config.font_size = 10
+config.font_size = 20
 config.color_scheme = "Catppuccin Macchiato"
 config.hide_tab_bar_if_only_one_tab = true
 
